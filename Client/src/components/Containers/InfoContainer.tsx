5c59260c77eca3b2ca835ab855299cac5e321ea3
Yishai Biton <yishai946@gmail.com>
2025-09-17 23:26:08 +0300

    feat: add InfoContainer component for structured content display

---
+++ Client/src/components/Containers/InfoContainer.tsx
@@ -0,0 +1,30 @@
+import { Stack, StackProps, Typography } from '@mui/material';
+import Row from './Row';
+
+interface InfoContainerProps extends StackProps {
+  children: React.ReactNode;
+  title: string;
+  icon?: React.ReactNode;
+}
+
+const InfoContainer = ({ children, title, icon, ...stackProps }: InfoContainerProps) => (
+  <Stack
+    {...stackProps}
+    direction="column"
+    display="flex"
+    p={3}
+    bgcolor="background.paper"
+    border="1px solid #cccccc8b"
+    borderRadius={2}
+  >
+    <Row alignItems="center" gap={1}>
+      {icon}
+      <Typography variant="h5" fontWeight="bold">
+        {title}
+      </Typography>
+    </Row>
+    {children}
+  </Stack>
+);
+
+export default InfoContainer;
