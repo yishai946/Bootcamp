9b11a48d6566e7a7545415e6df77a91d7e1d9cd0
Yishai Biton <yishai946@gmail.com>
2025-09-21 08:37:03 +0300

    feat: update RecruitExercises dates and implement loading/error states in Tasks page

--- Client/src/pages/Exercises/index.tsx
+++ Client/src/pages/Exercises/index.tsx
@@ -4,6 +4,8 @@ import useGetUserExercises from '@hooks/Exercises/useGetUserExercises';
 import { useUser } from '@providers/UserProvider';
 import ExerciseCard from './ExerciseCard';
 import { ExerciseStatus } from '@enums/ExerciseStatus';
+import ExercisesPageSkeleton from '@skeletons/PageSkeleton';
+import ErrorAlert from '@components/ErrorAlert';
 
 const Tasks = () => {
   const { user } = useUser();
@@ -21,7 +23,11 @@ const Tasks = () => {
     retry: teamRetry,
   } = useGetTeamExercises(user?.teamId || '');
 
-  return (
+  return teamLoading || recruitLoading ? (
+    <ExercisesPageSkeleton />
+  ) : teamError || recruitError ? (
+    <ErrorAlert error={teamError || recruitError} retry={teamError ? teamRetry : recruitRetry} />
+  ) : (
     <Column gap={4} alignItems="center" width="100%" mt={4}>
       {teamExercises.map((exercise) => (
         <ExerciseCard
---
+++ Client/src/skeletons/PageSkeleton.tsx
@@ -0,0 +1,12 @@
+import Column from '@components/Containers/Column';
+import { Skeleton } from '@mui/material';
+
+const ExercisesPageSkeleton = () => (
+  <Column gap={4} alignItems="center" width="100%" mt={4}>
+    {[...Array(5)].map((_, index) => (
+      <Skeleton key={index} variant="rounded" width="90%" height={68} />
+    ))}
+  </Column>
+);
+
+export default ExercisesPageSkeleton;
